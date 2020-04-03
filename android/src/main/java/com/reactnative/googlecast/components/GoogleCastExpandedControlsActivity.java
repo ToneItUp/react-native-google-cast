bc3b0a1b896a54e77639db30dbe3ed9de777d3fc
James Relyea <james.relyea@zifiniti.com>
2020-04-03 11:50:31 -0400

    Fix expanded controls on Android.

--- android/src/main/java/com/reactnative/googlecast/components/GoogleCastExpandedControlsActivity.java
+++ android/src/main/java/com/reactnative/googlecast/components/GoogleCastExpandedControlsActivity.java
@@ -1,21 +1,20 @@
 package com.reactnative.googlecast.components;
 
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import android.view.Menu;
+import com.reactnative.googlecast.R;
+
 
 public class GoogleCastExpandedControlsActivity
     extends ExpandedControllerActivity {
 
-  //   private static final int MENU_CAST = Menu.FIRST;
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      super.onCreateOptionsMenu(menu);
+      getMenuInflater().inflate(R.menu.expanded_controller, menu);
+      CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item);
+      return true;
+  }
 
-  //   @Override
-  //   public boolean onCreateOptionsMenu(Menu menu) {
-  //     super.onCreateOptionsMenu(menu);
-  //     menu.add(0, MENU_CAST, Menu.NONE, "Cast");
-  //     MenuItem item = menu.findItem(MENU_CAST);
-  //     MenuItemCompat.setActionProvider(
-  //         item, new MediaRouteActionProvider(getApplicationContext()));
-  //     // getMenuInflater().inflate(R.menu.expanded_controller, menu);
-  //     CastButtonFactory.setUpMediaRouteButton(this, menu, MENU_CAST);
-  //     return true;
-  //   }
 }
